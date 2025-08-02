49d72b82b0ea4a607deab20831767ecce7446acb
axolot33l <107714833+axolot33l@users.noreply.github.com>
2025-08-02 09:01:53 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
     idea
     java
-    id("gg.essential.loom") version "0.10.0"
+    id("gg.essential.loom") version "0.10.0.4"
     id("dev.architectury.architectury-pack200") version "0.1.3"
     id("com.github.johnrengelman.shadow") version "7.1.2"
 }
