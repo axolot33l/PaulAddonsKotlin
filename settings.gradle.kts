1746ca64f7cb01ce59aa134f230c88016c5987d9
axolot33l <107714833+axolot33l@users.noreply.github.com>
2025-08-02 09:19:17 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,7 +7,7 @@ pluginManagement {
         maven("https://maven.fabricmc.net")
         maven("https://maven.minecraftforge.net/")
         maven("https://repo.spongepowered.org/maven/")
-        maven("https://repo.polyfrost.cc/releases"
+        maven("https://repo.polyfrost.cc/releases")
         maven("https://maven.essential.gg/releases/")
         maven("https://repo.sk1er.club/repository/maven-public/") // keep this last
     }
